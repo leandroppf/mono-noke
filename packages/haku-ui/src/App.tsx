4c32050ff4f9019a5d9a572c21b7602000867f1a
Leandro Pedroso <lpedroso@meudroz.com>
2022-10-16 16:05:38 -0300

    Creating first component

--- packages/haku-ui/src/App.tsx
+++ packages/haku-ui/src/App.tsx
@@ -1,7 +1,17 @@
+import { TextInput } from "./lib";
+
 export function App() {
   return (
     <div className="p-8">
-      <h1>Hello World</h1>
+      <h1>Haku UI</h1>
+      <br />
+      <h2>Components</h2>
+      <br />
+      <h3>Text Input</h3>
+      <br />
+      <TextInput.Root>
+        <TextInput.Input />
+      </TextInput.Root>
     </div>
   );
 }
---
+++ packages/haku-ui/src/lib/index.ts
@@ -0,0 +1,8 @@
+// Inputs
+// TextInput
+export { default as TextInput } from "./Inputs/TextInput";
+export type {
+  TextInputIconProps,
+  TextInputInputProps,
+  TextInputRootProps,
+} from "./Inputs/TextInput";
